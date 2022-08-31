852cb1bd111491587ddbfd0ba302c08ecbf77b9a
manikchand.p <manikchand.p@zohocorp.com>
2022-08-31 12:44:28 +0530

    hateoas integration

--- src/main/java/com/example/manik/payroll/PayrollApplication.java
+++ src/main/java/com/example/manik/payroll/PayrollApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PayrollApplication {
 
 	public static void main(String[] args) {
+//		Employee e = new Employee("ganesh", "mls");
+//		System.out.println(e.getId());
 		SpringApplication.run(PayrollApplication.class, args);
 	}
 
